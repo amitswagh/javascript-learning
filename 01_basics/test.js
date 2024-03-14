829129f8b9758fda0321279dda6356e6a0678490
amitswagh <162913698+amitswagh@users.noreply.github.com>
2024-03-14 15:38:39 +0000

    adding variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,15 @@
-console.log("Amit")
+const accountId = 144550
+
+let accountEmail = "amitwagh29@google.com"
+var accountPassword = '12345'
+accountCity = 'Akola'
+
+accountEmail = 'amit76@google.com'
+accountPassword = '232323'
+accountCity = 'Pune'
+let accountState // Value = undefined
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* Don't use var
+Beacause of an issue of block scope and functional scope*/
